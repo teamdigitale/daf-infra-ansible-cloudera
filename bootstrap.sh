607425be87de0162fa3fe48230416b6de62cb383
Luca Prete <preteluca@gmail.com>
2019-05-31 17:24:08 +0200

    [DAFINFRA-138] Fix ansible repo references in bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,9 @@ yum -y update
 yum install -y git2u ansible
 
 # Clone daf-infra-ansible-cloudera repository
-rm -rf daf-infra-cloudera-bootstrap
+rm -rf daf-infra-cloudera
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone https://github.com/teamdigitale/daf-infra-cloudera.git
 
-pushd daf-infra-cloudera-bootstrap
+pushd daf-infra-cloudera
 
 ansible-playbook bootstrap.yml -i inventory/hosts-dev.yml
