de81b16524342e028d111efabd0c10b039265d95
Luca Prete <preteluca@gmail.com>
2019-05-31 09:04:19 +0200

    [DAFINFRA-138] Add env specific inventory

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ yum -y update
 yum install -y git2u ansible
 
 # Clone daf-infra-ansible-cloudera repository
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone git@github.com:teamdigitale/daf-infra-ansible-cloudera.git
+rm -rf daf-infra-cloudera-bootstrap
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone git@github.com:teamdigitale/daf-infra-cloudera-bootstrap.git
 
-ansible-playbook bootstrap.yml
+pushd daf-infra-cloudera-bootstrap
+
+ansible-playbook bootstrap.yml -i inventory/hosts-dev.yml
